94f568af8242c48ae9c01eadeab4da136aa7e500
Sergey Kuzenny <kuzenny4@gmail.com>
2021-03-26 19:02:15 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{2, 3, 4};
+        List<Integer> list = Lists.asList(1, arr);
+        System.out.println(list);
+    }
+}
